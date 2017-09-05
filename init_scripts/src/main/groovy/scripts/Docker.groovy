72c6810629b05c6c19fcaf9ccab6799c37de5f9c
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-09-05 15:53:01 +0200

    Define LCCI Docker images

--- init_scripts/src/main/groovy/scripts/Docker.groovy
+++ init_scripts/src/main/groovy/scripts/Docker.groovy
@@ -7,28 +7,18 @@ import org.jenkinsci.plugins.yad.DockerCloudHelper
 
 println("=== Installing Docker Cloud for Linux nodes")
 
-// Default agent image
-final DockerSlaveTemplate defaultJnlpAgentTemplate = DockerCloudHelper.fromTemplate("jenkins/jnlp-slave")
-defaultJnlpAgentTemplate.with {
-    // Default label to "docker", no caching
-    // User - jenkins (default)
+final DockerSlaveTemplate fusesocIcarus =
+    DockerCloudHelper.fromTemplate("onenashev/fusesoc-icarus").with {
+        labelString = "docker-fusesoc-icarus";
+        remoteFs = "/fusesoc"
+        ((DockerComputerJNLPLauncher)launcher).user = "root"
 }
 
-// Custom image for Maven builds
-MavenInstallation.DescriptorImpl mavenDescriptor = Jenkins.instance.getDescriptorByType(MavenInstallation.DescriptorImpl.class);
-final DockerSlaveTemplate mavenBuilderTemplate = DockerCloudHelper.fromTemplate("onenashev/demo-jenkins-maven-builder")
-mavenBuilderTemplate.with {
-    labelString = "docker linux mvnBuilder"
-    remoteFs = "/root"
-    ((DockerComputerJNLPLauncher)launcher).user = "root"
-    //TODO: Make volume names configurable
-    dockerContainerLifecycle.createContainer.volumes = ["maven-repo:/root/.m2", "jar-cache:/root/.jenkins"]
-    nodeProperties = [
-        new ToolLocationNodeProperty(
-            // Maven from the parent Maven image, we do not want to run the installer each time
-            new ToolLocationNodeProperty.ToolLocation(mavenDescriptor,"mvn", "/usr/share/maven")
-        )
-    ]
-}
+final DockerSlaveTemplate ciModules =
+    DockerCloudHelper.fromTemplate("librecores/ci-modules").with {
+        labelString = "docker-fusesoc-icarus";
+        remoteFs = "/fusesoc"
+        ((DockerComputerJNLPLauncher)launcher).user = "root"
+    }
 
-DockerCloudHelper.setup([defaultJnlpAgentTemplate, mavenBuilderTemplate])
+DockerCloudHelper.setup([fusesocIcarus, ciModules])
